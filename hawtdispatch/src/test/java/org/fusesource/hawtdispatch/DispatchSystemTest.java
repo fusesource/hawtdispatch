09f09bffc316ae52cb2dda5081e7ec979afa4c6e
Hiram Chirino <hiram@hiramchirino.com>
2012-02-01 06:30:10 -0500

    Updating lic. Headers.

--- hawtdispatch/src/test/java/org/fusesource/hawtdispatch/DispatchSystemTest.java
+++ hawtdispatch/src/test/java/org/fusesource/hawtdispatch/DispatchSystemTest.java
@@ -1,13 +1,12 @@
 /**
- * Copyright (c) 2008-2009 Apple Inc. All rights reserved.
- * Copyright (C) 2009-2010, Progress Software Corporation and/or its
- * subsidiaries or affiliates.  All rights reserved.
+ * Copyright (C) 2012 FuseSource, Inc.
+ * http://fusesource.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.fusesource.hawtdispatch;
 
 import java.util.concurrent.CountDownLatch;
